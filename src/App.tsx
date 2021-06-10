6603d5c07fa2191905d3a2b25df137d7166aae8e
Raihan Jamil <raihanjamil841@gmail.com>
2021-06-10 17:30:54 +0600

    jamil

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Login from './components/Login';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <Login />
+  );
+}
+
+export default App;
+
